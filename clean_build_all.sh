a4b2d345c8e06de5b6d83a207c7ed532ea020207
Hannes Schnaitter <hannes.schnaitter@gmail.com>
2024-11-17 07:58:19 +0100

    Update: Konsolidierung der build-Schritte in einem Skirpt

--- clean_build_all.sh
+++ clean_build_all.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
-bash ./clean_build_de.sh
-bash ./clean_build_en.sh
 
+echo "Clean all"
+jupyter-book clean de
+jupyter-book clean en
+
+echo "Build languages"
+jupyter-book build --path-output _de ./de
+jupyter-book build --path-output _en ./en
+
+echo "Clean previous output"
 rm -rf _html
 mkdir -p _html
 mkdir -p _html/de
 mkdir -p _html/en
+
+echo "Prepare _html"
 cp index.html _html/.
 cp -r _de/_build/html/* _html/de
 cp -r _en/_build/html/* _html/en
